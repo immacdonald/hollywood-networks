e1be8b607bad108e36bd9f51cd6cc9fcad33e6d3
Ian MacDonald <ianmacdonaldm@gmail.com>
2023-08-05 20:17:06 -0700

    Added Test page for routing testing

---
+++ src/components/Test.tsx
@@ -0,0 +1,10 @@
+function Test() {
+    return (
+        <div>
+            <h1>Test</h1>
+            <p>Testing React router. {import.meta.env.BASE_URL}</p>
+        </div>
+    );
+}
+
+export default Test;
--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename={`/${process.env.PUBLIC_URL}`}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
