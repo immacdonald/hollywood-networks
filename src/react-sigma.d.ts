a9a9c959d7c0303e61fdf0aca7466ebfd7d86559
Ian MacDonald <ianmacdonaldm@gmail.com>
2023-08-16 01:34:08 -0400

    Fixed TypeScript error from react-sigma

---
+++ src/react-sigma.d.ts
@@ -0,0 +1,25 @@
+// Fix from for react-sigma typing from https://github.com/dunnock/react-sigma/issues/86#issuecomment-728873673
+
+declare module "react-sigma";
+
+type SigmaGraph = {
+    nodes: SigmaNode[],
+    edges: SigmaEdge[]
+}
+
+type SigmaNode = {
+    id: string;
+    label: string;
+    x?: number,
+    y?: number,
+    size?: number,
+    color?: string
+}
+
+type SigmaEdge = {
+    id: string;
+    source: string;
+    target: string;
+    label?: string,
+    color?: string;
+}
