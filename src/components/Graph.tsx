f5bb7802630edb4efe0cd82de58432250b6dcfee
Ian MacDonald <ianmacdonaldm@gmail.com>
2023-08-16 02:02:59 -0400

    Testing base url for public assets

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -5,9 +5,15 @@ import {Sigma, LoadJSON} from 'react-sigma'
 const NetworkGraph: FC = () => {
     // Create the graph
 
+    let baseURL = import.meta.env.BASE_URL;
+    if(!baseURL.endsWith("/")) {
+        baseURL = baseURL + "/";
+    }
+    console.log(baseURL)
+
     return (
         <Sigma style={{width:"100%", height:"600px", backgroundColor: "transparent", border: "1px solid black"}}>
-            <LoadJSON path={String(import.meta.env.BASE_URL) + "dataset.json"} />
+            <LoadJSON path={baseURL + "dataset.json"} />
         </Sigma>
     );
 };
