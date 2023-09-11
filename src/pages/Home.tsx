f06604b5633e58ad7037e3c361d6b52d0cee30a0
Ian MacDonald <ianmacdonaldm@gmail.com>
2023-09-10 23:34:08 -0400

    Updates to name text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import NetworkGraph from '../components/NetworkGraph';
 function Home() {
     return (
         <div>
-            <h1>Hollywood Director & Crew Networks</h1>
+            <h1>Hollywood Director-Crew Networks</h1>
             <p>
                 This is an exploration of how directors in Hollywood use similar crew members throughout multiple
                 productions.
