30eca35c9279f381fd775d3706568294efe56af3
Ian MacDonald <ianmacdonaldm@gmail.com>
2023-10-02 11:05:54 -0400

    Created a dynamic graph legend

--- src/components/NetworkGraph.tsx
+++ src/components/NetworkGraph.tsx
@@ -12,6 +12,7 @@ type NodeAttributes = {
     [index: number]: number;
     size: number;
     label: string;
+    color: string;
 };
 
 type EdgeAttributes = {
@@ -44,10 +45,34 @@ interface GraphData {
 const NetworkGraph: React.FC<unknown> = () => {
     const graphData: GraphData = jsonGraph as unknown as GraphData;
 
+    const graphLegend: Map<string, { color: string, count : number }> = new Map<string, { color: string, count : number }>;
+
     // Scale each node to have the size of cust_size attribute within a normalized range
     graphData.nodes.forEach((node, index) => {
         graphData.nodes[index].attributes.size = Math.max(node.attributes[2] / 50, 1);
+        graphData.nodes[index].attributes.label = node.attributes[1] + " (" + node.attributes[0] + ")";
+        
+        const identity : string = String(node.attributes[0]);
+
+        if(!(graphLegend.get(identity))) {
+            graphLegend.set(identity, { color: node.attributes.color, count: 1});
+        } else {
+            const existing : { color : string, count : number } | undefined = graphLegend.get(identity);
+            if(existing) {
+                existing.count++;
+                graphLegend.set(identity, existing);
+            }
+        }
     });
+
+    const legendList : Array<{ key : string, color : string, count : number }> = [];
+
+    graphLegend.forEach((value : { color : string, count : number}, key : string)=>{
+        legendList.push({key, ...value});
+      })
+
+    legendList.sort((a, b) => b.count-a.count);
+    console.log(legendList);
 
     // Size each edge based on the weight and update the edge color to be translucent
     graphData.edges.forEach((edge, index) => {
@@ -77,36 +102,48 @@ const NetworkGraph: React.FC<unknown> = () => {
     };
 
     return (
-        <div className={style.graph}>
-            <SigmaContainer
-                className={style.sigmaGraph}
-                settings={{
-                    nodeProgramClasses: { image: getNodeProgramImage() },
-                    labelDensity: 0.07,
-                    labelGridCellSize: 60,
-                    labelRenderedSizeThreshold: 15,
-                    labelFont: 'Lato, sans-serif',
-                    zIndex: true
-                }}
-            >
-                <GraphDefault order={100} probability={0.1} graph={graph}/>
-                <ControlsContainer
-                    position={'bottom-right'}
-                    style={{ position: 'absolute', bottom: '32px', left: '16px' }}
+        <div className={style.fullGraph}>        
+            <div className={style.graph}>
+                <SigmaContainer
+                    className={style.sigmaGraph}
+                    settings={{
+                        nodeProgramClasses: { image: getNodeProgramImage() },
+                        labelDensity: 0.07,
+                        labelGridCellSize: 60,
+                        labelRenderedSizeThreshold: 15,
+                        labelFont: 'Lato, sans-serif',
+                        zIndex: true
+                    }}
                 >
-                    <ZoomControl />
-                    <FullScreenControl />
-                </ControlsContainer>
-                <ControlsContainer
-                    position={"top-left"}
-                    style={{ position: 'absolute', bottom: '8px', left: '16px' }}
-                >
-                    <SearchControl style={{ width: "200px" }} />
-                </ControlsContainer>
+                    <GraphDefault order={100} probability={0.1} graph={graph}/>
+                    <ControlsContainer
+                        position={'bottom-right'}
+                        style={{ position: 'absolute', bottom: '32px', left: '16px' }}
+                    >
+                        <ZoomControl />
+                        <FullScreenControl />
+                    </ControlsContainer>
+                    <ControlsContainer
+                        position={"top-left"}
+                        style={{ position: 'absolute', bottom: '8px', left: '16px' }}
+                    >
+                        <SearchControl style={{ width: "200px" }} />
+                    </ControlsContainer>
 
-                <GraphEvents />
-            </SigmaContainer>
+                    <GraphEvents />
+                </SigmaContainer>
+            </div>
+            <div className={style.legend}>
+                <p><strong>Legend</strong></p>
+                {legendList.map((legend) => {
+                    return <div className={style.legendKey}>
+                        {legend.key.toUpperCase()}
+                        <span style={{ backgroundColor : legend.color}}></span>
+                    </div>;
+                })}
+            </div>
         </div>
+
     );
 };
 
