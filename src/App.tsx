54c85de552390460200acd002ecadc925a673c64
Ian MacDonald <ianmacdonaldm@gmail.com>
2023-10-27 21:45:17 -0400

    Fixed TypeScript unused variable error

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Header from './components/Header';
-import Footer from './components/Footer';
+//import Footer from './components/Footer';
 import 'katex/dist/katex.min.css';
 
 function App() {
