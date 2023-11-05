99b051b6e00266b2356c4559afe19e76f93aa284
Ian MacDonald <ianmacdonaldm@gmail.com>
2023-11-04 22:22:33 -0400

    Added logo click link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,14 @@
 import DynamicLink from './DynamicLink';
+import { Link } from 'react-router-dom';
 import style from './Header.module.scss';
 
 function Header() {
     return (
         <header className={style.header}>
             <div className={style.content}>
-                <div className={style.logo}>Hollywood Director-Crew Networks</div>
+                <div className={style.logo}>
+                    <Link to="/">Hollywood Director-Crew Networks</Link>
+                </div>
                 <div className={style.navigation}>
                     <nav className={style.navLinks}>
                         <DynamicLink link="/" label="Home" />
